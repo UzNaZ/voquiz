0c9f7699a8cf6d98b9b2de5378e939f077618244
UzNaZ <uzik.bs@gmail.com>
2025-06-27 20:27:24 +0200

    fixed excessive RAM usage

--- backend/src/utils/serializers.py
+++ backend/src/utils/serializers.py
@@ -1,4 +1,5 @@
 import re
+import typing
 
 from backend.values import AllRegexes
 
@@ -15,34 +16,34 @@ SpreadsheetRowWithParens = tuple[WordFieldWithParens, WordFieldWithParens]
 def check_for_multiple_words(
     spreadsheet_data: list[SpreadsheetRow], separator: str = ","
 ) -> list[SpreadsheetRowWithMultipleTranslations]:
-    edited_data = list(spreadsheet_data)
-    for i, (en_word, uk_word) in enumerate(edited_data):
+    spreadsheet_data = typing.cast(list[SpreadsheetRowWithMultipleTranslations], spreadsheet_data)
+    for i, (en_word, uk_word) in enumerate(spreadsheet_data):
         if separator in uk_word:
             uk_words = [word.strip() for word in uk_word.split(separator)]
-            edited_data[i] = (en_word, uk_words)
+            spreadsheet_data[i] = (en_word, uk_words)
 
-    for i, (en_word, uk_word) in enumerate(edited_data):
+    for i, (en_word, uk_word) in enumerate(spreadsheet_data):
         if separator in en_word:
             en_words = [word.strip() for word in en_word.split(separator)]
-            edited_data[i] = (en_words, uk_word)
+            spreadsheet_data[i] = (en_words, uk_word)
 
-    return edited_data
+    return spreadsheet_data
 
 
 def check_for_parentheses(
     spreadsheet_data: list[SpreadsheetRow],
 ) -> list[SpreadsheetRowWithParens]:
-    edited_data = list(spreadsheet_data)
-    for i, (en_word, uk_word) in enumerate(edited_data):
+    spreadsheet_data = typing.cast(list[SpreadsheetRowWithParens], spreadsheet_data)
+    for i, (en_word, uk_word) in enumerate(spreadsheet_data):
         if "(" in en_word:
             new_en_word = split_by_parentheses(en_word)
-            edited_data[i] = (new_en_word, uk_word)
+            spreadsheet_data[i] = (new_en_word, uk_word)
 
-    for i, (en_word, uk_word) in enumerate(edited_data):
+    for i, (en_word, uk_word) in enumerate(spreadsheet_data):
         if "(" in uk_word:
             new_uk_word = split_by_parentheses(uk_word)
-            edited_data[i] = (en_word, new_uk_word)
-    return edited_data
+            spreadsheet_data[i] = (en_word, new_uk_word)
+    return spreadsheet_data
 
 
 def split_by_parentheses(text: str) -> WordTuple:
