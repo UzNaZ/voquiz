710ece2f5997c0d8e12573b153170bb0f700b405
UzNaZ <uzik.bs@gmail.com>
2025-07-02 19:57:05 +0200

    pre deploy fixes

--- backend/src/api/routes/start_menu.py
+++ backend/src/api/routes/start_menu.py
@@ -5,16 +5,13 @@ from fastapi.templating import Jinja2Templates
 from backend.src.models.schemas.pydantic_schemas.quiz_data import QuizData
 from backend.src.services.redis import SessionData, get_session_data
 from backend.src.utils.validators import validate_quiz_form
-from backend.values import RedisData
 
 menu_router = APIRouter()
 templates = Jinja2Templates(directory="frontend/templates")
 
 
 @menu_router.get("/")
-async def start(
-    request: Request, session_data: SessionData = Depends(get_session_data)
-):
+async def start(request: Request):
     return templates.TemplateResponse(name="home.html", request=request)
 
 
